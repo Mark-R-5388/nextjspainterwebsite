3fc81c38d6db27738557914cbf61d6728d69d814
Mark-R-5388 <markrose@markwebdev.com>
2022-03-17 11:11:03 -0500

    fixed layout

--- components/Card.js
+++ components/Card.js
@@ -1,27 +1,29 @@
-import Image from "next/image";
-import styles from "../styles/Card.module.css";
+import Image from 'next/image';
+import styles from '../styles/Card.module.css';
 
 const Card = ({ image, alt }) => {
-  return (
-    <div className={styles.card_container}>
-      <div className={styles.card_image_container}>
-        <Image
-          priority
-          className={styles.card_image}
-          src={image}
-          alt={alt}
-          layout="fill"
-        />
-      </div>
-      <div className={styles.card_text_container}>
-        <h3 className={styles.card_text_header}>Header 3</h3>
-        <p className={styles.card_text_paragraph}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui
-          molestiae, adipisci pariatur aspernatur sit porro?
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.card_container}>
+			<div className={styles.card_image_container}>
+				<Image
+					priority
+					className={styles.card_image}
+					src={image}
+					alt={alt}
+					layout='fill'
+				/>
+			</div>
+			<div className={styles.card_text_container}>
+				<h3 className={styles.card_text_header}>Header 3</h3>
+				<p className={styles.card_text_paragraph}>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+				</p>
+				<p className={styles.card_text_paragraph}>
+					Qui molestiae, adipisci pariatur aspernatur sit porro?
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
